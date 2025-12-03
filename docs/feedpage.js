6f726b373d0e7784f27ac8667bbf06e4989e98ad
diyab123 <DiyaBasnet@my.unt.edu>
2025-12-03 12:03:14 -0600

    Update feedpage.js

--- docs/feedpage.js
+++ docs/feedpage.js
@@ -1,16 +1,19 @@
-<script>
-  function AddPost() {
+
+  function addPost() {
     const name = document.getElementById("spotName").value;
     const description = document.getElementById("description").value;
-    const tagList = document.getElementById("tags").value.split(","); 
+    const tags = document.getElementById("tags").value; 
+    const rating = document.getElemenetByid("rating").value; 
+    
+  if (!name || !desc) { 
+    alert("Spot Name and Description are required.");
+    return; 
   } 
 
-const newPost = { 
-  name,
-  description, 
-  rating 
-}; 
-
+  const feed = document.getElementById("feed"); 
+  
+const post = document.createElement("div"); 
+    post.classList.add("post-card");
 function renderFeed() { 
       const feed = document.getElementById("feed");
       feed.innerHTML =""; 
@@ -21,6 +24,4 @@ function renderFeed() {
         <p>${post.desc}</p>
         <div class ="tags">${tagsHTML}</div>
         <p class="rating">Rating: ${post.rating}</p>
-
-  </script>
 
