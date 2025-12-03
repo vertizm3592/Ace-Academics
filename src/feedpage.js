1d273f2161f0949aef48bb0f69150b1129bd63b2
diyab123 <DiyaBasnet@my.unt.edu>
2025-12-03 09:45:02 -0600

    Update feedpage.js

--- src/feedpage.js
+++ src/feedpage.js
@@ -1,5 +1,9 @@
 <script>
   function AddPost() {
+    const name = document.getElementById("spotName").value;
+    const desc = document.getElementById("description").value;
+    const tagList = document.getElementById("tags").value.split(","); 
+
     
   } 
   </script>
