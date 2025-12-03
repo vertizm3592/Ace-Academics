0078d328ac439ca3edb5584e3d9092263fb6237f
diyab123 <DiyaBasnet@my.unt.edu>
2025-12-03 10:10:57 -0600

    Update feedpage.js

--- src/feedpage.js
+++ src/feedpage.js
@@ -1,13 +1,26 @@
 <script>
   function AddPost() {
     const name = document.getElementById("spotName").value;
-    const desc = document.getElementById("description").value;
+    const description = document.getElementById("description").value;
     const tagList = document.getElementById("tags").value.split(","); 
   } 
 
-    function renderFeed() { 
+const newPost = { 
+  name,
+  description, 
+  rating 
+}; 
+
+function renderFeed() { 
       const feed = document.getElementById("feed");
       feed.innerHTML =""; 
+
+      const card = 
+        <div class="card"> 
+        <h3>${post.name}</h3>
+        <p>${post.desc}</p>
+        <div class ="tags">${tagsHTML}</div>
+        <p class="rating">Rating: ${post.rating}</p>
 
   </script>
 
